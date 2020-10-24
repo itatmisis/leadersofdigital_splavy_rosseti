19a3715847cc4330a84b45ac55b021fc0c9a714a
sostema <sostema@gmail.com>
2020-10-25 00:56:02 +0300

    dockerfile

--- api/ml.py
+++ api/ml.py
@@ -1,6 +1,5 @@
-import datetime
-
 import pandas as pd
+from comtrade import Comtrade
 
 
 class DetectPhase:
@@ -16,63 +15,72 @@ class DetectPhase:
         start = 0
         st_bool = True
         end = 0
+        A = 0.5
         short_type = '1_faza'
-        try:
-            for index, row in faza_1.iterrows():
-                if row['3U0 ВЛ-330-09 330 kV'] > 100:
-                    if st_bool:
-                        start = row['t, сек.']
-                        st_bool = False
-                    end = row['t, сек.']
-            return short_type, start, end
-        except:
-            return 0, 0, 0
+        for index, row in faza_1.iterrows():
+            if abs(row['Ub']) < 20 and abs(row['Ib']) > 1 or abs(row['Ua']) < 20 and abs(row['Ia']) > 1 or abs(row['Uc']) < 20 and abs(row['Ic']) > 1:
+                if st_bool:
+                    start = row['t']
+                    st_bool = False
+                end = row['t']
+        return (short_type, start, end)
 
     @staticmethod
     def time_2_faza(faza_2):
         start = 0
         st_bool = True
         end = 0
-        a = 1
+        A = 0.5
         short_type = '2_faza'
-        try:
-            for index, row in faza_2.iterrows():
-                if (row['Ia ВЛ-29 токи'] > a) | (row['Ia ВЛ-29 токи'] < -a):
-                    if st_bool:
-                        start = row['t, сек.']
-                        st_bool = False
-                    end = row['t, сек.']
-            return short_type, start, end
-        except:
-            return 0, 0, 0
+        for index, row in faza_2.iterrows():
+            a = abs(row['Ia']) > A
+            b = abs(row['Ib']) > A
+            c = abs(row['Ic']) > A
+            if ((a and b and not c) or (a and c and not b) or (b and c and not a)):
+                if st_bool:
+                    start = row['t']
+                    st_bool = False
+                end = row['t']
+        return (short_type, start, end)
 
     @staticmethod
     def time_3_faza(faza_3):
         start = 0
         st_bool = True
         end = 0
-        a = 1
+        A = 0.5
         short_type = '3_faza'
-        try:
-            for index, row in faza_3.iterrows():
-                if (row['Ia ВЛ-330-Дерб. 330 kV'] > a) | (row['Ia ВЛ-330-Дерб. 330 kV'] < -a):
-                    if st_bool:
-                        start = row['t, сек.']
-                        st_bool = False
-                    end = row['t, сек.']
-            return short_type, start, end
-        except:
-            return 0, 0, 0
+        for index, row in faza_3.iterrows():
+            a = abs(row['Ia']) > A
+            b = abs(row['Ib']) > A
+            c = abs(row['Ic']) > A
+            if a and b and c:
+                if st_bool:
+                    start = row['t']
+                    st_bool = False
+                end = row['t']
+        return (short_type, start, end)
 
     @staticmethod
-    def detect(path_to_csv):
-        try:
-            data = pd.read_csv(path_to_csv)
-            res = DetectPhase.time_1_faza(data)
-            if res[1] == 0 and res[2] == 0:
-                res = DetectPhase.time_2_faza(data)
-                if res[1] == 0 and res[2] == 0:
-                    res = DetectPhase.time_3_faza(data)
-            return res
-        except:
-            return 1, datetime.datetime(2020, 5, 17), datetime.datetime(2020, 5, 18)
+    def detect(path_to_cff, k_stat=1):
+        rec = Comtrade()
+        rec.load(path_to_cff)
+        keys = ['t'] + rec.analog_channel_ids
+        values = [rec.time] + rec.analog
+        d = dict(zip(keys,values))
+        data = pd.DataFrame(data = d)
+        col_pref = ['Ua', 'Ub', 'Uc', '3U0', 'Ia', 'Ib', 'Ic', '3I0']
+        col_name  = ['t']
+        col_name += k_stat * col_pref
+        data.columns = col_name
+        data = data.dropna()
+        results = []
+        for i in range(k_stat):
+            phase = pd.concat([data.iloc[:, 0], data.iloc[:, 1 + i * 8: 9 + i * 8]], axis=1, sort=False)
+            res = DetectPhase.time_1_faza(phase)
+            if res[1] == 0  and res[2] == 0:
+                res = DetectPhase.time_3_faza(phase)
+                if res[1] == 0  and res[2] == 0:
+                    res = DetectPhase.time_2_faza(phase)
+            results.append(res)
+        return results
