641d1ce5b198a9d12832c553fbbddfb9f52e66b9
Eighonet <31664867+Eighonet@users.noreply.github.com>
2020-10-25 14:50:30 +0300

    frontend_release

--- frontend/js/main_map.js
+++ frontend/js/main_map.js
@@ -28,25 +28,44 @@
              infowindow.open(map, marker);
          });
      }
-     alert(JSON.stringify(data.data))
+//     alert(JSON.stringify(data.data[0].events[0].probability))
 //     alert(JSON.stringify(parseFloat(data.data[0].events[0].markers[0].geographic_location.latitude)))
 //     alert(JSON.stringify(data.data[0].events[0].markers[0].geographic_location.longitude))
-     alert(data.data.length);
-     for (let i = 0; i < data.data.length; i++) {
-         const location = { lat:   parseFloat(JSON.stringify(data.data[i].events[0].markers[0].geographic_location.latitude)), lng: parseFloat(JSON.stringify(data.data[i].events[0].markers[0].geographic_location.longitude))};
 
-         const contentString =
-             '<div id="content">' +
-             '<div id="siteNotice">' +
-             "</div>" +
-             '<h1 id="firstHeading" class="firstHeading">ВКБ-1123</h1>' +
-             '<div id="bodyContent">' +
-             "<p>Тип КЗ:" +  + "</p>" +
-             "<p>Длительность КЗ: 43 мс</p>" +
-             "<p>Время: 12:54:13:1032 - 12:54:13:1075</p>" +
-             "<p>Вероятность срабатывания защиты: 0.71</p>" +
-             "</div>" +
-             "</div>";
+//     alert(data.data.length);
+     for (let i = 0; i < data.data.length; i++) {
+         const location = { lat:   parseFloat(JSON.stringify(data.data[i].geographic_location.latitude)), lng: parseFloat(JSON.stringify(data.data[i].geographic_location.longitude))};
+         let contentString = "";
+         if (data.data[i].events.length != 0) {
+             contentString =
+                 '<div id="content">' +
+                 '<div id="siteNotice">' +
+                 "</div>" +
+                 '<h1 id="firstHeading" class="firstHeading">' + data.data[i].title.toString() + '</h1>' +
+                 '<div id="bodyContent">' +
+                 "<p>Тип КЗ: " + data.data[i].events[0].event_type.toString() +"</p>" +
+                 "<p>Длительность КЗ: " + data.data[i].events[0].event_length + " мс.</p>" +
+                 "<p>Время: " + data.data[i].events[0].event_start + "</p>" +
+                 "<p>К-во ошибок: " + data.data[i].events[0].probability + "</p>" +
+                 "</div>" +
+                 "</div>";
+
+
+                let ul = document.getElementById("myUL");
+                let li = document.createElement("li");
+                let a = document.createElement("a");
+                a.appendChild(document.createTextNode( data.data[i].title.toString() + " - " + data.data[i].events[0].event_start))
+                li.appendChild(a);
+                ul.appendChild(li);
+
+         } else {
+             contentString = '<div id="content">' +
+                 '<div id="siteNotice">' +
+                 "</div>" +
+                 '<div id="firstHeading" class="firstHeading">' +
+                 data.data[i].title.toString() + '</div>' +
+                 "</div>";
+         }
          addMarker(location, contentString);
  }
 
